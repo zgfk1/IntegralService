7888be5839151daabcbf84dd20e30b3bc25c174c
han <hzh00112@qq.com>
2019-07-23 11:33:29 +0800

    添加初始Controller 添加数据库对应实体

---
+++ src/main/java/com/czbank/integralservice/domain/User.java
@@ -0,0 +1,13 @@
+package com.czbank.integralservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+}
