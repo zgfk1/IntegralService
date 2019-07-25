77acda2f7b2bee681918ee63bc08825d79ef9e83
sithope <270563860@qq.com>
2019-07-25 09:29:30 +0800

    Push误操作，已删除。

--- IntegralService/src/main/java/com/czbank/integralservice/controller/MallController.java
+++
@@ -1,8 +0,0 @@
-package com.czbank.integralservice.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MallController {
-
-}
--- IntegralService/src/main/java/com/czbank/integralservice/controller/MissionController.java
+++
@@ -1,7 +0,0 @@
-package com.czbank.integralservice.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MissionController {
-}
