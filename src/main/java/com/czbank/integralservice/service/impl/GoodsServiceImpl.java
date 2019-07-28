323eace77ad51509f824c08a0f5427ea7c9d31d9
sanji1010 <740129325@qq.com>
2019-07-28 23:16:40 +0800

    加个小功能

--- src/main/java/com/czbank/integralservice/service/impl/GoodsServiceImpl.java
+++ src/main/java/com/czbank/integralservice/service/impl/GoodsServiceImpl.java
@@ -1,7 +1,9 @@
 package com.czbank.integralservice.service.impl;
 
 import com.czbank.integralservice.mapper.CommodityMapper;
+import com.czbank.integralservice.mapper.ExchangeMapper;
 import com.czbank.integralservice.model.Commodity;
+import com.czbank.integralservice.model.Exchange;
 import com.czbank.integralservice.service.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,10 @@ public class GoodsServiceImpl implements GoodsService {
 
     @Autowired
     private CommodityMapper commodityrMapper;
+
+    @Autowired
+    private ExchangeMapper exchangeMapper;
+
     @Override
     public int insert(Commodity commodity) {
         return commodityrMapper.insert(commodity);
@@ -39,10 +45,23 @@ public class GoodsServiceImpl implements GoodsService {
     }
 
     @Override
-    public List<Commodity> selectAllPage( int np, int size){ return commodityrMapper.selectAllPage((np-1)*size,size); }
+    public List<Commodity> selectAllPage(int np, int size) {
+        return commodityrMapper.selectAllPage((np - 1) * size, size);
+    }
 
     @Override
-    public int count(){ return commodityrMapper.count(); }
+    public int count() {
+        return commodityrMapper.count();
+    }
 
+    @Override
+    public int countUserGoods(String userId){
+        return exchangeMapper.countUserGoods(userId);
+    }
+
+    @Override
+    public List<Exchange> selectAllUserGoods(String userId){
+        return exchangeMapper.selectAllUserGoods(userId);
+    }
 }
 
